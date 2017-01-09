62b51899f469d3c51a4c9767936d12771fa77418
Grace Ning <gracening97@gmail.com>
2017-01-09 11:04:25 -0500

    Initial commit

---
+++ src/app/snek.ts
@@ -0,0 +1,7 @@
+var app = angular.module("snek", []);
+
+app.controller("SnekCtrl", ["$scope", "$timeout", "$log", function ($scope, $timeout, $log) {
+    $log.debug("Initializing Controller");
+    
+
+}]);
